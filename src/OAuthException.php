7ecc1d29cb39a7868967e33f24a64a8d236b0e1a
Dan Perron <danp3rr0n@gmail.com>
2014-08-29 01:52:32 -0400

    license

--- src/OAuthException.php
+++ src/OAuthException.php
@@ -5,6 +5,7 @@ namespace danperron\OAuth;
  * Description of OAuthException
  *
  * @author Dan Perron <danp3rr0n@gmail.com>
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
  */
 class OAuthException extends \Exception {
     public function __construct($message, $code = 0, $previous = null) {
